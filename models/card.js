45474695252416cc41c32fab0d14a3e733b547f5
Vadim Markelov <Vedmakmarkelov@yandex.ru>
2023-04-13 13:29:18 +0300

    run test

--- models/card.js
+++ models/card.js
@@ -12,7 +12,7 @@ const cardSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator(url) {
-        const urlRegex = /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&=]*)$/;
+        const urlRegex = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-/]))?/;
         return urlRegex.test(url);
       },
       message: 'Некорректный url',
